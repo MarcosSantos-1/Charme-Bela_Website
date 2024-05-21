adaecae77a94d153c0ed49067e1155a5837f6e09
Marcos Vinicius <marcosmacos@Mac-Pro-de-Marcos.local>
2024-05-20 22:00:37 -0700

    app finished 1.0

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
   AOS.init();
-  
-/*
-  function myMenuFunction() {
-    var menuBtn = document.getElementById("myNavMenu");
 
-    if (menuBtn.className === "nav-menu") {
-        menuBtn.className += " responsive";
-    } else {
-        menuBtn.className = "nav-menu";
-    }
-}
-
-function closeMenu() {
-    var menuBtn = document.getElementById("myNavMenu");
-    menuBtn.className = "nav-menu";
-}
-
-// Adiciona um event listener a todos os itens de menu para fechar o menu ao clicar
-document.querySelectorAll('.nav-item').forEach(item => {
-    item.addEventListener('click', closeMenu);
-});
-
-*/
   const menu = document.querySelector('.menu');
   const NavMenu = document.querySelector('.nav-menu');
   
@@ -43,3 +21,41 @@ document.querySelectorAll('.nav-item').forEach(item => {
 function sended(){
   alert("E-mail enviado com sucesso!")
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const viewer = document.getElementById('viewer');
+  const viewerImg = document.getElementById('viewer-img');
+  const closeBtn = document.querySelector('.close');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');
+  const images = document.querySelectorAll('.grid-item');
+  let currentIndex = 0;
+
+  function showImage(index) {
+      viewerImg.src = images[index].src;
+  }
+  images.forEach((item, index) => {
+      item.addEventListener('click', () => {
+          currentIndex = index;
+          viewer.style.display = 'flex';
+          showImage(currentIndex);
+      });
+  });
+  closeBtn.addEventListener('click', () => {
+      viewer.style.display = 'none';
+  });
+  viewer.addEventListener('click', (e) => {
+      if (e.target === viewer) {
+          viewer.style.display = 'none';
+      }
+  });
+  prevBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      showImage(currentIndex);
+  });
+  nextBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % images.length;
+      showImage(currentIndex);
+  });
+});
